6b487476acdacbaa94a32c4d9a02dc56fd9f4cb8
huwen <huwen@huwendeMacBook-Pro.local>
2017-09-08 18:31:03 +0800

    degisnmode

---
+++ src/Observer/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Subject interface {
+	Register()
+	RemoverRegister()
+	Notify()
+}
+
+type Observer interface {
+	Name() string
+	SayHello() string
+}
+
+type SubjA struct {
+	obserMap map[string]Observer
+}
+
+func (s *SubjA) Register(o Observer) {
+	key := o.Name()
+	if s.obserMap == nil {
+		s.obserMap = make(map[string]Observer, 0)
+	}
+	s.obserMap[key] = o
+}
+
+func (s *SubjA) RemoverRegister(o Observer) {
+	key := o.Name()
+	delete(s.obserMap, key)
+}
+
+func (s *SubjA) Notify() {
+	for _, o := range s.obserMap {
+		fmt.Println(o.SayHello())
+	}
+}
+
+type ObsA struct {
+	TagName string
+}
+
+func (o *ObsA) Name() string {
+	return o.TagName
+}
+
+func (o *ObsA) SayHello() string {
+	return "hello, i am " + o.Name()
+}
+
+func main() {
+	s := &SubjA{}
+	o1 := &ObsA{"jack"}
+	o2 := &ObsA{"jhon"}
+	o3 := &ObsA{"mike"}
+	s.Register(o1)
+	s.Register(o2)
+	s.Register(o3)
+	s.Notify()
+}
